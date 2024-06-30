55833fa66c0412abfe8c4bf61a56bcffedf82d35
ileostar <hi@leostar.top>
2024-06-30 23:22:46 +0800

    chore: update code

--- README.md
+++ README.md
@@ -5,6 +5,11 @@ leostar's vscode plugin
 ## 功能需求
 
 - 常用的代码片段
-- 快捷重构代码
-- 快速删除当前函数
-- 常用拓展包
+
+```txt
+vbase
+
+vbase-less
+
+vbase-sass
+```
--- eslint.config.ts
+++
@@ -1,9 +0,0 @@
-import antfu from '@antfu/eslint-config'
-
-export default antfu({
-  markdown: true,
-  typescript: {
-    tsconfigPath: 'tsconfig.json',
-  },
-  stylistic: true,
-})
